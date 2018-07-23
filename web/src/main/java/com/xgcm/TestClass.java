42b2bbddcb135e0c16312dc1341c64db9f7209f5
yxg19990601 <1758644275@qq.com>
2018-07-23 23:27:38 +0800

    Initial commit

---
+++ web/src/main/java/com/xgcm/TestClass.java
@@ -0,0 +1,26 @@
+package com.xgcm;
+
+/**
+ * @Author YXG
+ * @Date 2018-07-23 21:13
+ */
+public class TestClass {
+
+    public void  method1() {
+        
+    }
+    public static void main(String[] args) {
+        TestClass testClass = new TestClass();
+        String[] strs = {",","2"};
+
+        for (String str : strs) {
+            System.out.println(str);
+        }
+        
+        eat();
+
+    }
+
+    private static void eat() {
+    }
+}
---
+++ web/src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello Worasdf</h2>
+</body>
+</html>
